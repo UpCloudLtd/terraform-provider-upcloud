603f96aad50c024da717f4568edf3fbddaf9465a
Niko Haapaviita <niko.haapaviita@viidakko.fi>
2018-10-16 16:39:04 +0300

    Created an list related on improvements of the provider * TODO list on the markdown file * An example terraform file related on the restructured provider

--- examples/01_server.tf
+++ examples/01_server.tf
@@ -6,8 +6,9 @@ provider "upcloud" {
 }
 
 resource "upcloud_server" "test" {
+  count    = "1"
   zone     = "fi-hel1"
-  hostname = "ubuntu.example.tld"
+  hostname = "nikoh-tf-provider-testing-${format("%04d", count.index + 1)}"
 
   cpu      = "2"
   mem      = "1024"
@@ -15,81 +16,30 @@ resource "upcloud_server" "test" {
   # Login details
   login {
     user = "tf"
-
     keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCYn6VuEgiH3//qpSa/b3Khrjy3Z4Q4fvvhRNRDrZaJqddLvQLCtoL2ktoke7+0jTcR4Vydi8bk8csUQlZxpWC6SIfif+tB8HjwusbUfLT5I5fJEI/O7gtktvtWkK4GnePFXYIdgKlXKRJ92xFnNOGV+el2zug78QahsrzsyV0Cucfjb7twPyojh5iPl3gf6f7NBHVnsqNELhJqmpo4uY+vSTfHx0siyIGP0U/Jz9dB64kbnoG6GL2fh3CEQ950Ll2luY/cfX52SO+WX/nl156A2VVCozkOSE3wbZ501Gd1508KY7ctuaqOue4DF8ZuQ1uzv4Lf9sfg4Bv4jBMTu4tvB",
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCYn6VuEgiH3//qpSa/b3Khrjy3Z4Q4fvvhRNRDrZaJqddLvQLCtoL2ktoke7+0jTcR4Vydi8bk8csUQlZxpWC6SIfif+tB8HjwusbUfLT5I5fJEI/O7gtktvtWkK4GnePFXYIdgKlXKRJ92xFnNOGV.............",
     ]
 
     create_password   = true
-    password_delivery = "sms"
+    password_delivery = "email"
   }
-
-  storage_devices = [
-    {
-      # You can use both storage template names and UUIDs
-      size    = 50
-      action  = "clone"
-      tier    = "maxiops"
-      storage = "Ubuntu Server 16.04 LTS (Xenial Xerus)"
-
-      backup_rule = {
-        interval = "daily"
-        time = "0100"
-        retention = 8
-      }
-    }
-  ]
-}
-
-resource "upcloud_server" "test2" {
-  zone     = "fi-hel1"
-  hostname = "ubuntu.example.tld"
-
-  plan     = "2xCPU-4GB"
-
-  # Login details
-  login {
-    user = "tf"
-
-    keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCYn6VuEgiH3//qpSa/b3Khrjy3Z4Q4fvvhRNRDrZaJqddLvQLCtoL2ktoke7+0jTcR4Vydi8bk8csUQlZxpWC6SIfif+tB8HjwusbUfLT5I5fJEI/O7gtktvtWkK4GnePFXYIdgKlXKRJ92xFnNOGV+el2zug78QahsrzsyV0Cucfjb7twPyojh5iPl3gf6f7NBHVnsqNELhJqmpo4uY+vSTfHx0siyIGP0U/Jz9dB64kbnoG6GL2fh3CEQ950Ll2luY/cfX52SO+WX/nl156A2VVCozkOSE3wbZ501Gd1508KY7ctuaqOue4DF8ZuQ1uzv4Lf9sfg4Bv4jBMTu4tvB",
-    ]
-
-    create_password   = true
-    password_delivery = "sms"
-  }
-
-  storage_devices = [
-    {
-      # You can use both storage template names and UUIDs
-      size    = 50
-      action  = "clone"
-      tier    = "maxiops"
-      storage = "01000000-0000-4000-8000-000020040100"
-
-    backup_rule = {
-        interval = "mon"
-        time = "0100"
-        retention = 2
-      }
-    }
-  ]
 }
 
 
-resource "upcloud_tag" "My-tag" {
-  name        = "TagName1"
-  description = "TagDescription"
-  servers     = [
-     "${upcloud_server.test.id}",
-     "${upcloud_server.test2.id}"
-  ]
+resource "upcloud_storage" "my-storage" {
+			size  = 10
+			tier  = "maxiops"
+			title = "My data collection"
+			zone  = "fi-hel1"
+      templatize = true
+      source_storage_id = "01000000-0000-4000-8000-000020040100"
+      instances = ["${upcloud_server.test.*.id}"]
+		}
+
+output "ipv4_addresses" {
+  value = ["${upcloud_server.test.*.ipv4_address}"]
 }
 
-output "test ipv4_address" {
-  value = "${upcloud_server.test.ipv4_address}"
-}
-
-output "test2 ipv4_address" {
-  value = "${upcloud_server.test2.ipv4_address}"
+output "storage_uuids" {
+  value = ["${upcloud_storage.my-storage.*.id}"]
 }
