af89d17b3eefe8c5975ef52e193828e3e1744cca
Touko Hallasmaa <touko.hallasmaa@upcloud.com>
2020-12-07 14:22:18 +0200

    fix: should pass `storage` when creating a template

--- upcloud/resource_upcloud_server.go
+++ upcloud/resource_upcloud_server.go
@@ -229,6 +229,7 @@ func resourceUpCloudServer() *schema.Resource {
 							Type:        schema.TypeString,
 							ForceNew:    true,
 							Optional:    true,
+							Required:    true,
 						},
 						"backup_rule": backupRuleSchema(),
 					},
