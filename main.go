d1a7db820ee6f22afd6da782a927d963335af024
AJ Kovalainen <ajk@upcloud.com>
2021-01-19 17:25:55 +0200

    chore: add "go generate" for generating docs

--- main.go
+++ main.go
@@ -6,6 +6,15 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
+// Run "go generate" to format example terraform files and generate the docs for the registry/website
+// IMPORTANT! the "go:generate" comments below are magic comments for "go generate" to function.
+
+// Ensure the documentation is formatted properly.
+//go:generate terraform fmt -recursive ./upcloud/
+
+// Run the docs generation tool
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
