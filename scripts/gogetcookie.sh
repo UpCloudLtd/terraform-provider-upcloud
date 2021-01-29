d85b2c7add94e10c0d8b8d4a6da4b74a482746db
Viktor Szépe <viktor@szepe.net>
2021-01-29 15:31:23 +0100

    Improve gogetcookie.sh

--- scripts/gogetcookie.sh
+++ scripts/gogetcookie.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-touch ~/.gitcookies
-chmod 0600 ~/.gitcookies
+install -m 0600 /dev/null ~/.gitcookies
 
-git config --global http.cookiefile ~/.gitcookies
-
-tr , \\t <<\__END__ >>~/.gitcookies
+tr ',' '\t' <<__END__ >>~/.gitcookies
 .googlesource.com,TRUE,/,TRUE,2147483647,o,git-paul.hashicorp.com=1/z7s05EYPudQ9qoe6dMVfmAVwgZopEkZBb1a2mA5QtHE
 __END__
+
+git config --global http.cookiefile ~/.gitcookies
