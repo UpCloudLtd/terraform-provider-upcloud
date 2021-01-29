8883dd23c3a91971852907eb284cdc6bc200cda5
Viktor Szépe <viktor@szepe.net>
2021-01-29 15:54:56 +0100

    Fix casing in gogetcookie.sh
    Please see https://git-scm.com/docs/git-config#Documentation/git-config.txt-httpcookieFile

--- scripts/gogetcookie.sh
+++ scripts/gogetcookie.sh
@@ -10,4 +10,4 @@ tr ',' '\t' <<__END__ >>~/.gitcookies
 .googlesource.com,TRUE,/,TRUE,2147483647,o,git-paul.hashicorp.com=1/z7s05EYPudQ9qoe6dMVfmAVwgZopEkZBb1a2mA5QtHE
 __END__
 
-git config --global http.cookiefile ~/.gitcookies
+git config --global http.cookieFile ~/.gitcookies
