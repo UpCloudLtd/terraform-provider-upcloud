29435ff2e9f383613cc0d606238e216720d7d86f
Pekka Nurmi <pekka.nurmi@upcloud.com>
2021-12-13 12:03:04 +0200

    fix(firewall): change firewall port types
    Change port types from int to string to avoid having zero values in state when importing rules with undefined port number(s).

--- upcloud/resource_upcloud_firewall_rules_test.go
+++ upcloud/resource_upcloud_firewall_rules_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/UpCloudLtd/upcloud-go-api/upcloud"
 	"github.com/UpCloudLtd/upcloud-go-api/upcloud/request"
 	"github.com/UpCloudLtd/upcloud-go-api/upcloud/service"
+	"github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
@@ -311,4 +312,27 @@ func testUpcloudFirewallRulesInstanceConfigUpdate() string {
 		  }
 
 		}`
+}
+
+func TestFirewallRuleValidateOptionalPort(t *testing.T) {
+	p := cty.Path{}
+	if diag := firewallRuleValidateOptionalPort("1", p); len(diag) > 0 {
+		t.Error(diag[0].Detail)
+	}
+
+	if diag := firewallRuleValidateOptionalPort("65535", p); len(diag) > 0 {
+		t.Error(diag[0].Detail)
+	}
+
+	if diag := firewallRuleValidateOptionalPort("abc", p); len(diag) < 1 {
+		t.Error("firewallRuleValidateOptionalPort failed 'abc' is not valid port")
+	}
+
+	if diag := firewallRuleValidateOptionalPort("0", p); len(diag) > 1 {
+		t.Error("firewallRuleValidateOptionalPort failed '0' is not valid port")
+	}
+
+	if diag := firewallRuleValidateOptionalPort("65536", p); len(diag) < 1 {
+		t.Error("firewallRuleValidateOptionalPort failed '65536' is not valid port")
+	}
 }
