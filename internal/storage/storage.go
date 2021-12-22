dda4ee0059b5231ddce3d5c2fdb4ec25ceaf09a7
Pekka Nurmi <pekka.nurmi@upcloud.com>
2021-12-22 11:22:11 +0200

    chore(docs): Move notes from document templates to resource descriptions.

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -7,10 +7,17 @@ import (
 
 func BackupRuleSchema() *schema.Schema {
 	return &schema.Schema{
-		Description: "The criteria to backup the storage",
-		Type:        schema.TypeList,
-		MaxItems:    1,
-		Optional:    true,
+		Description: `The criteria to backup the storage  
+		Please keep in mind that it's not possible to have a server with backup_rule attached to a server with simple_backup specified.
+		Such configurations will throw errors during execution.  
+		Also, due to how UpCloud API works with simple backups and how Terraform orders the update operations, 
+		it is advised to never switch between simple_backup on the server and individual storages backup_rules in one apply.
+		If you want to switch from using server simple backup to per-storage defined backup rules, 
+		please first remove simple_backup block from a server, run 'terraform apply', 
+		then add 'backup_rule' to desired storages and run 'terraform apply' again.`,
+		Type:     schema.TypeList,
+		MaxItems: 1,
+		Optional: true,
 		Elem: &schema.Resource{
 			Schema: map[string]*schema.Schema{
 				"interval": {
