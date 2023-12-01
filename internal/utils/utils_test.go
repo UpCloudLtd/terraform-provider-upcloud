8fb3e6c38353f21302dcd223027a9cf39cc742de
Ville Välimäki <ville.valimaki@upcloud.com>
2023-12-01 13:50:38 +0200

    test(marshal): move to Utils pkg

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -107,3 +107,33 @@ func TestStorageAddressFormat(t *testing.T) {
 	ret = StorageAddressFormat(storageAddressEmpty)
 	assert.Equal(t, ret, "")
 }
+
+func TestMarshalID(t *testing.T) {
+	want := "load/balancer/frontend"
+	got := MarshalID("load", "balancer", "frontend")
+	if want != got {
+		t.Errorf("marshalID failed want %s got %s", want, got)
+	}
+}
+
+func TestUnmarshalID(t *testing.T) {
+	var load, balancer, frontend string
+	id := "load/balancer/frontend"
+	err := UnmarshalID(id, &load, &balancer, &frontend)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if load != "load" {
+		t.Errorf("utils.UnmarshalID failed want load got %s", load)
+	}
+	if balancer != "balancer" {
+		t.Errorf("utils.UnmarshalID failed want balancer got %s", balancer)
+	}
+	if frontend != "frontend" {
+		t.Errorf("utils.UnmarshalID failed want frontend got %s", frontend)
+	}
+	err = UnmarshalID(id, &load, &balancer)
+	if err == nil {
+		t.Fatal("utils.UnmarshalID failed expected 'not enough components' error got nil")
+	}
+}
