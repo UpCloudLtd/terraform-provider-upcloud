d6f5cade5330aed98522be1d3b474ecc761a29c9
Toni Kangas <toni.kangas@upcloud.com>
2024-03-01 14:43:42 +0200

    test: validate docs subcategories

--- scripts/group-docs.sh
+++ scripts/group-docs.sh
@@ -1,4 +1,6 @@
-#!/bin/sh -xe
+#!/bin/sh -e
+
+subcategory_missing=""
 
 for d in resources data-sources; do
     files=$(ls "docs/$d")
@@ -9,5 +11,15 @@ for d in resources data-sources; do
         if [ "$subcategory" != "null" ]; then
             sed -i "s/^subcategory:.*/subcategory: $subcategory/" docs/$d/$f;
         fi;
+
+        if [ "$subcategory" = "null" ]; then
+            subcategory_missing="${subcategory_missing}$d/$f\n"
+        fi;
     done;
 done;
+
+if [ -n "$subcategory_missing" ]; then
+    echo "Error: subcategory missing for:\n"
+    echo "${subcategory_missing}"
+    exit 1
+fi;
