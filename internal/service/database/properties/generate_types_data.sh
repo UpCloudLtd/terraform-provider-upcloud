b5ac2220062c3362afdee3bb1fd188c578ef921d
Toni Kangas <toni.kangas@upcloud.com>
2024-03-11 14:26:20 +0200

    chore: document `jq` usage

--- internal/service/database/properties/generate_types_data.sh
+++ internal/service/database/properties/generate_types_data.sh
@@ -7,4 +7,5 @@ if [ -z "$UPCLOUD_USERNAME" ] || [ -z "$UPCLOUD_PASSWORD" ]; then
     exit 1
 fi
 
+# Fetch DB types data and remove extra fields and whitespace. End-result is a single line JSON file with DB properties objects by database type.
 upctl database types -o json | jq -cM 'map_values({properties})' > $target
