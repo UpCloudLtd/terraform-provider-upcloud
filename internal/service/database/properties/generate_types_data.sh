9eb3c6d646e4587282b500c35648c5f9cb12e416
Toni Kangas <kangasta@users.noreply.github.com>
2024-07-02 11:02:18 +0300

    ci: add workflow to automatically run `make generate` (#575)
    Also updates the DB properties generate script so that differences are presented in human readable format.

--- internal/service/database/properties/generate_types_data.sh
+++ internal/service/database/properties/generate_types_data.sh
@@ -7,5 +7,19 @@ if [ -z "$UPCLOUD_USERNAME" ] || [ -z "$UPCLOUD_PASSWORD" ]; then
     exit 1
 fi
 
+cat $target | jq > old_$target
+
 # Fetch DB types data and remove extra fields and whitespace. End-result is a single line JSON file with DB properties objects by database type.
 upctl database types -o json | jq -cM 'map_values({properties})' > $target
+
+cat $target | jq > new_$target
+
+if diff -u old_$target new_$target > $target.diff; then
+    echo "No changes detected."
+else
+    echo "Changes detected. Please review the diff file: $target.diff"
+    echo ""
+    cat $target.diff
+fi
+
+rm -f old_$target new_$target
