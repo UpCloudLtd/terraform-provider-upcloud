109b976bd881c966a7f564957575bc7955235d97
Toni Kangas <toni.kangas@upcloud.com>
2024-09-10 16:23:21 +0300

    test(objsto): add tests for custom domains

---
+++ upcloud/testdata/upcloud_managed_object_storage/managed_object_storage_custom_domain_s3.tf
@@ -0,0 +1,28 @@
+variable "prefix" {
+  default = "tf-acc-test-objsto2-custom-domain-"
+  type    = string
+}
+
+variable "zone" {
+  default = "fi-hel1"
+  type    = string
+}
+
+variable "region" {
+  default = "europe-1"
+  type    = string
+}
+
+resource "upcloud_managed_object_storage" "this" {
+  name              = "${var.prefix}-objsto"
+  region            = var.region
+  configured_status = "started"
+
+  network {
+    family = "IPv4"
+    name   = "public"
+    type   = "public"
+  }
+}
+
+// Delete the custom domain
