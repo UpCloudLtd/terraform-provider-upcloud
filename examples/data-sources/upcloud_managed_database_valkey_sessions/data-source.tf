c0b2e82664e4caa7ed8f11bf55e17e5592665205
Toni Kangas <toni.kangas@upcloud.com>
2024-11-14 14:22:47 +0200

    chore(dbaas): add examples for valkey resource and sessions data-source

---
+++ examples/data-sources/upcloud_managed_database_valkey_sessions/data-source.tf
@@ -0,0 +1,14 @@
+# Use data source to gather a list of the active sessions for a Managed Valkey Database
+
+# Create a Managed Redis resource
+resource "upcloud_managed_database_valkey" "example" {
+  name  = "example"
+  title = "example"
+  plan  = "1x1xCPU-2GB"
+  zone  = "fi-hel2"
+}
+
+# Read the active sessions of the newly created service
+data "upcloud_managed_database_valkey_sessions" "example" {
+  service = upcloud_managed_database_valkey.example.id
+}
