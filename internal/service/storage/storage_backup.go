10aad962cf435030cdf02244fd76d5845d1db097
Francisco Serrano <59340762+paketeserrano@users.noreply.github.com>
2025-05-06 15:31:40 +0200

    feat(storage backup): Add computed values (#762)
    * feat(storage backup): Add computed values
    
    * Update internal/service/storage/storage_backup.go
    
    Co-authored-by: Toni Kangas <kangasta@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Toni Kangas <kangasta@users.noreply.github.com>

--- internal/service/storage/storage_backup.go
+++ internal/service/storage/storage_backup.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -41,9 +42,8 @@ func (r *storageBackupResource) Configure(_ context.Context, req resource.Config
 
 type storageBackupModel struct {
 	SourceStorage types.String `tfsdk:"source_storage"`
-	Title         types.String `tfsdk:"title"`
-	ID            types.String `tfsdk:"id"`
 	CreatedAt     types.String `tfsdk:"created_at"`
+	storageCommonModel
 }
 
 func (r *storageBackupResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
@@ -71,6 +71,40 @@ func (r *storageBackupResource) Schema(_ context.Context, _ resource.SchemaReque
 			"created_at": schema.StringAttribute{
 				Computed:    true,
 				Description: "Timestamp of the backup creation.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"encrypt": schema.BoolAttribute{
+				MarkdownDescription: encryptDescription,
+				Computed:            true,
+			},
+			"labels":        utils.ReadOnlyLabelsAttribute("storage"),
+			"system_labels": utils.SystemLabelsAttribute("storage"),
+			"size": schema.Int64Attribute{
+				MarkdownDescription: sizeDescription,
+				Computed:            true,
+				PlanModifiers: []planmodifier.Int64{
+					int64planmodifier.UseStateForUnknown(),
+				},
+			},
+			"tier": schema.StringAttribute{
+				MarkdownDescription: tierDescription,
+				Computed:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"type": schema.StringAttribute{
+				MarkdownDescription: typeDescription,
+				Computed:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"zone": schema.StringAttribute{
+				MarkdownDescription: "The zone the storage is in, e.g. `de-fra1`.",
+				Computed:            true,
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.UseStateForUnknown(),
 				},
@@ -111,7 +145,7 @@ func (r *storageBackupResource) Create(ctx context.Context, req resource.CreateR
 	}
 
 	data.CreatedAt = types.StringValue(backupDetails.Created.Format(time.RFC3339))
-	data.ID = types.StringValue(backupDetails.UUID)
+	resp.Diagnostics.Append(setCommonValues(ctx, &data.storageCommonModel, &backupDetails.Storage)...)
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
@@ -144,9 +178,8 @@ func (r *storageBackupResource) Read(ctx context.Context, req resource.ReadReque
 		return
 	}
 
-	data.ID = types.StringValue(backupDetails.UUID)
 	data.CreatedAt = types.StringValue(backupDetails.Created.Format(time.RFC3339))
-	data.Title = types.StringValue(backupDetails.Title)
+	resp.Diagnostics.Append(setCommonValues(ctx, &data.storageCommonModel, &backupDetails.Storage)...)
 
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
