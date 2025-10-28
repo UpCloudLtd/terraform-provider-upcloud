6e8e17d3ffc31539dafa5b3ed131d3daa5535063
Francisco Serrano <59340762+paketeserrano@users.noreply.github.com>
2025-10-28 13:59:13 +0200

    feat: file storage support (#853)
    * feat: add file storage support
    
    * feat: add support for file storage
    
    * feat: support labels in file storage service
    
    * feat: latest bug fixing and acceptance tests
    
    * chore(file storage): add file storage example
    
    * chore: move tests config to separate config files
    
    * chore: change network object to a set
    
    * chore: mark file storage as experimental

---
+++ upcloud/testdata/upcloud_file_storage/file_storage_cfg2.tf
@@ -0,0 +1,84 @@
+
+variable "prefix" {
+  default = "tf-acc-test-file-storage-"
+  type    = string
+}
+
+variable "net-name" {
+  default = "net-name"
+  type    = string
+}
+
+variable "file-storage-name" {
+  default = "file-storage-name"
+  type    = string
+}
+
+variable "cidr" {
+  default = "172.16.34.0/24"
+  type    = string
+}
+
+variable "acl-1-ip" {
+  default = "172.16.34.45"
+  type    = string
+}
+
+variable "acl-2-ip" {
+  default = "172.16.34.46"
+  type    = string
+}
+
+variable "network-ip-addrs" {
+  default = "172.16.34.50"
+  type    = string
+}
+
+resource "upcloud_network" "this" {
+  name = "${var.prefix}${var.net-name}"
+  zone = "fi-hel2"
+
+  ip_network {
+    address = "${var.cidr}"
+    dhcp    = true
+    family  = "IPv4"
+  }
+}
+
+resource "upcloud_file_storage" "example" {
+  name              = "${var.prefix}${var.file-storage-name}-2"
+  size              = 250
+  zone              = "fi-hel2"
+  configured_status = "started"
+
+  labels = {
+    environment = "staging"
+    customer    = "example-customer"
+    env         = "test"
+  }
+
+  share {
+    name = "write-to-project"
+    path = "/project"
+    acl {
+      target     = "${var.acl-1-ip}"
+      permission = "rw"
+    }
+  }
+
+  share {
+    name = "read-only"
+    path = "/public"
+    acl {
+      target     = "${var.acl-2-ip}"
+      permission = "ro"
+    }
+  }
+
+  network {
+    family     = "IPv4"
+    name       = "example-private-net"
+    uuid       = upcloud_network.this.id
+    ip_address = "${var.network-ip-addrs}"
+  }
+}
