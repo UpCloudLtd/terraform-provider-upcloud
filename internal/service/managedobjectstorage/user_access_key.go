152dbe2ffd6662a7fb1e73bdb24dfada926f0e51
Toni Kangas <toni.kangas@upcloud.com>
2025-10-30 15:16:57 +0200

    chore(objsto): migrate user access key resource to pf

--- internal/service/managedobjectstorage/user_access_key.go
+++ internal/service/managedobjectstorage/user_access_key.go
@@ -4,202 +4,261 @@ import (
 	"context"
 
 	"github.com/UpCloudLtd/terraform-provider-upcloud/internal/utils"
-
 	"github.com/UpCloudLtd/upcloud-go-api/v8/upcloud"
 	"github.com/UpCloudLtd/upcloud-go-api/v8/upcloud/request"
 	"github.com/UpCloudLtd/upcloud-go-api/v8/upcloud/service"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-func ResourceManagedObjectStorageUserAccessKey() *schema.Resource {
-	return &schema.Resource{
-		Description:   "This resource represents an UpCloud Managed Object Storage user access key.",
-		CreateContext: resourceManagedObjectStorageUserAccessKeyCreate,
-		ReadContext:   resourceManagedObjectStorageUserAccessKeyRead,
-		UpdateContext: resourceManagedObjectStorageUserAccessKeyUpdate,
-		DeleteContext: resourceManagedObjectStorageUserAccessKeyDelete,
-		Importer: &schema.ResourceImporter{
-			StateContext: schema.ImportStatePassthroughContext,
-		},
-		Schema: map[string]*schema.Schema{
-			"access_key_id": {
-				Description: "Access key id.",
+var (
+	_ resource.Resource                = &managedObjectStorageUserAccessKeyResource{}
+	_ resource.ResourceWithConfigure   = &managedObjectStorageUserAccessKeyResource{}
+	_ resource.ResourceWithImportState = &managedObjectStorageUserAccessKeyResource{}
+)
+
+func NewManagedObjectStorageUserAccessKeyResource() resource.Resource {
+	return &managedObjectStorageUserAccessKeyResource{}
+}
+
+type managedObjectStorageUserAccessKeyResource struct {
+	client *service.Service
+}
+
+func (r *managedObjectStorageUserAccessKeyResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_managed_object_storage_user_access_key"
+}
+
+// Configure adds the provider configured client to the resource.
+func (r *managedObjectStorageUserAccessKeyResource) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	r.client, resp.Diagnostics = utils.GetClientFromProviderData(req.ProviderData)
+}
+
+type userAccessKeyModel struct {
+	AccessKeyID     types.String `tfsdk:"access_key_id"`
+	CreatedAt       types.String `tfsdk:"created_at"`
+	ID              types.String `tfsdk:"id"`
+	LastUsedAt      types.String `tfsdk:"last_used_at"`
+	SecretAccessKey types.String `tfsdk:"secret_access_key"`
+	ServiceUUID     types.String `tfsdk:"service_uuid"`
+	Status          types.String `tfsdk:"status"`
+	Username        types.String `tfsdk:"username"`
+}
+
+func (r *managedObjectStorageUserAccessKeyResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Description: "This resource represents an UpCloud Managed Object Storage user access key.",
+		Attributes: map[string]schema.Attribute{
+			"access_key_id": schema.StringAttribute{
+				Description: "Access key ID.",
 				Computed:    true,
-				Type:        schema.TypeString,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
 			},
-			"created_at": {
+			"created_at": schema.StringAttribute{
 				Description: "Creation time.",
 				Computed:    true,
-				Type:        schema.TypeString,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
 			},
-			"last_used_at": {
+			"id": schema.StringAttribute{
+				Description: "ID of the user. ID is in {object storage UUID}/{username}/{access key id} format.",
+				Computed:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"last_used_at": schema.StringAttribute{
 				Description: "Last used.",
 				Computed:    true,
-				Type:        schema.TypeString,
 			},
-			"secret_access_key": {
+			"secret_access_key": schema.StringAttribute{
 				Description: "Secret access key.",
 				Computed:    true,
 				Sensitive:   true,
-				Type:        schema.TypeString,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
 			},
-			"service_uuid": {
+			"service_uuid": schema.StringAttribute{
 				Description: "Managed Object Storage service UUID.",
 				Required:    true,
-				ForceNew:    true,
-				Type:        schema.TypeString,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
 			},
-			"status": {
-				Description:  "Status of the key. Valid values: `Active`|`Inactive`",
-				Required:     true,
-				Type:         schema.TypeString,
-				ValidateFunc: validation.StringInSlice([]string{string(upcloud.ManagedObjectStorageUserAccessKeyStatusActive), string(upcloud.ManagedObjectStorageUserAccessKeyStatusInactive)}, false),
+			"status": schema.StringAttribute{
+				Description: "Status of the key. Valid values: `Active`|`Inactive`",
+				Required:    true,
+				Validators: []validator.String{
+					stringvalidator.OneOf(
+						string(upcloud.ManagedObjectStorageUserAccessKeyStatusActive),
+						string(upcloud.ManagedObjectStorageUserAccessKeyStatusInactive),
+					),
+				},
 			},
-			"username": {
+			"username": schema.StringAttribute{
 				Description: "Username.",
 				Required:    true,
-				ForceNew:    true,
-				Type:        schema.TypeString,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
 			},
 		},
 	}
 }
 
-func resourceManagedObjectStorageUserAccessKeyCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) (diags diag.Diagnostics) {
-	svc := meta.(*service.Service)
+func setUserAccessKeyValues(_ context.Context, data *userAccessKeyModel, accessKey *upcloud.ManagedObjectStorageUserAccessKey) diag.Diagnostics {
+	var respDiagnostics diag.Diagnostics
 
-	req := &request.CreateManagedObjectStorageUserAccessKeyRequest{
-		Username:    d.Get("username").(string),
-		ServiceUUID: d.Get("service_uuid").(string),
+	data.AccessKeyID = types.StringValue(accessKey.AccessKeyID)
+	data.CreatedAt = types.StringValue(accessKey.CreatedAt.String())
+	data.LastUsedAt = types.StringValue(accessKey.LastUsedAt.String())
+
+	if accessKey.SecretAccessKey != nil {
+		data.SecretAccessKey = types.StringValue(*accessKey.SecretAccessKey)
 	}
 
-	accessKey, err := svc.CreateManagedObjectStorageUserAccessKey(ctx, req)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	d.SetId(utils.MarshalID(req.ServiceUUID, req.Username, accessKey.AccessKeyID))
-
-	status := upcloud.ManagedObjectStorageUserAccessKeyStatus(d.Get("status").(string))
-	if status != accessKey.Status {
-		accessKey, err = svc.ModifyManagedObjectStorageUserAccessKey(ctx, &request.ModifyManagedObjectStorageUserAccessKeyRequest{
-			Username:    d.Get("username").(string),
-			ServiceUUID: d.Get("service_uuid").(string),
-			AccessKeyID: accessKey.AccessKeyID,
-			Status:      status,
-		})
-		if err != nil {
-			return diag.FromErr(err)
-		}
-	}
-
-	return setManagedObjectStorageUserAccessKeyData(d, accessKey)
+	return respDiagnostics
 }
 
-func resourceManagedObjectStorageUserAccessKeyRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	svc := meta.(*service.Service)
+func (r *managedObjectStorageUserAccessKeyResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data userAccessKeyModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
 
-	var serviceUUID, username, id string
-	if err := utils.UnmarshalID(d.Id(), &serviceUUID, &username, &id); err != nil {
-		return diag.FromErr(err)
+	if resp.Diagnostics.HasError() {
+		return
 	}
 
-	// If service UUID is not set already set it based on the Id. This is the case for example when importing existing user access key.
-	if _, ok := d.GetOk("service_uuid"); !ok {
-		err := d.Set("service_uuid", serviceUUID)
-		if err != nil {
-			return diag.FromErr(err)
-		}
+	apiReq := &request.CreateManagedObjectStorageUserAccessKeyRequest{
+		Username:    data.Username.ValueString(),
+		ServiceUUID: data.ServiceUUID.ValueString(),
 	}
 
-	// If username is not set already set it based on the Id. This is the case for example when importing existing user access key.
-	if _, ok := d.GetOk("username"); !ok {
-		err := d.Set("username", username)
-		if err != nil {
-			return diag.FromErr(err)
-		}
+	accessKey, err := r.client.CreateManagedObjectStorageUserAccessKey(ctx, apiReq)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to create managed object storage user access key",
+			utils.ErrorDiagnosticDetail(err),
+		)
+		return
 	}
 
-	accessKey, err := svc.GetManagedObjectStorageUserAccessKey(ctx, &request.GetManagedObjectStorageUserAccessKeyRequest{
-		ServiceUUID: serviceUUID,
+	data.ID = types.StringValue(utils.MarshalID(data.ServiceUUID.ValueString(), data.Username.ValueString(), accessKey.AccessKeyID))
+
+	resp.Diagnostics.Append(setUserAccessKeyValues(ctx, &data, accessKey)...)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *managedObjectStorageUserAccessKeyResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var data userAccessKeyModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	if data.ID.ValueString() == "" {
+		resp.State.RemoveResource(ctx)
+		return
+	}
+
+	var serviceUUID, username, accessKeyID string
+	err := utils.UnmarshalID(data.ID.ValueString(), &serviceUUID, &username, &accessKeyID)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to unmarshal managed object storage user access key ID",
+			utils.ErrorDiagnosticDetail(err),
+		)
+		return
+	}
+
+	data.ServiceUUID = types.StringValue(serviceUUID)
+	data.Username = types.StringValue(username)
+	data.AccessKeyID = types.StringValue(accessKeyID)
+
+	user, err := r.client.GetManagedObjectStorageUserAccessKey(ctx, &request.GetManagedObjectStorageUserAccessKeyRequest{
 		Username:    username,
-		AccessKeyID: id,
+		ServiceUUID: serviceUUID,
+		AccessKeyID: accessKeyID,
 	})
 	if err != nil {
-		return utils.HandleResourceError(d.Get("access_key_id").(string), d, err)
-	}
-
-	return setManagedObjectStorageUserAccessKeyData(d, accessKey)
-}
-
-func resourceManagedObjectStorageUserAccessKeyUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) (diags diag.Diagnostics) {
-	if !d.HasChange("status") {
-		return nil
-	}
-
-	svc := meta.(*service.Service)
-
-	var serviceUUID, username, id string
-	if err := utils.UnmarshalID(d.Id(), &serviceUUID, &username, &id); err != nil {
-		return diag.FromErr(err)
-	}
-
-	req := &request.ModifyManagedObjectStorageUserAccessKeyRequest{
-		Status:      upcloud.ManagedObjectStorageUserAccessKeyStatus(d.Get("status").(string)),
-		ServiceUUID: serviceUUID,
-		Username:    username,
-		AccessKeyID: id,
-	}
-
-	accessKey, err := svc.ModifyManagedObjectStorageUserAccessKey(ctx, req)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	return setManagedObjectStorageUserAccessKeyData(d, accessKey)
-}
-
-func resourceManagedObjectStorageUserAccessKeyDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	svc := meta.(*service.Service)
-
-	var serviceUUID, username, id string
-	if err := utils.UnmarshalID(d.Id(), &serviceUUID, &username, &id); err != nil {
-		return diag.FromErr(err)
-	}
-
-	req := &request.DeleteManagedObjectStorageUserAccessKeyRequest{
-		ServiceUUID: serviceUUID,
-		Username:    username,
-		AccessKeyID: id,
-	}
-
-	if err := svc.DeleteManagedObjectStorageUserAccessKey(ctx, req); err != nil {
-		return diag.FromErr(err)
-	}
-
-	return nil
-}
-
-func setManagedObjectStorageUserAccessKeyData(d *schema.ResourceData, accessKey *upcloud.ManagedObjectStorageUserAccessKey) (diags diag.Diagnostics) {
-	if err := d.Set("access_key_id", accessKey.AccessKeyID); err != nil {
-		return diag.FromErr(err)
-	}
-	if err := d.Set("created_at", accessKey.CreatedAt.String()); err != nil {
-		return diag.FromErr(err)
-	}
-	if err := d.Set("status", accessKey.Status); err != nil {
-		return diag.FromErr(err)
-	}
-	if err := d.Set("last_used_at", accessKey.LastUsedAt.String()); err != nil {
-		return diag.FromErr(err)
-	}
-	if accessKey.SecretAccessKey != nil {
-		if err := d.Set("secret_access_key", accessKey.SecretAccessKey); err != nil {
-			return diag.FromErr(err)
+		if utils.IsNotFoundError(err) {
+			resp.State.RemoveResource(ctx)
+		} else {
+			resp.Diagnostics.AddError(
+				"Unable to read managed object storage user access key details",
+				utils.ErrorDiagnosticDetail(err),
+			)
 		}
+		return
 	}
 
-	return diags
+	resp.Diagnostics.Append(setUserAccessKeyValues(ctx, &data, user)...)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *managedObjectStorageUserAccessKeyResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var data userAccessKeyModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	accessKey, err := r.client.ModifyManagedObjectStorageUserAccessKey(ctx, &request.ModifyManagedObjectStorageUserAccessKeyRequest{
+		ServiceUUID: data.ServiceUUID.ValueString(),
+		Username:    data.Username.ValueString(),
+		AccessKeyID: data.AccessKeyID.ValueString(),
+		Status:      upcloud.ManagedObjectStorageUserAccessKeyStatus(data.Status.ValueString()),
+	})
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to update managed object storage user access key",
+			utils.ErrorDiagnosticDetail(err),
+		)
+		return
+	}
+
+	resp.Diagnostics.Append(setUserAccessKeyValues(ctx, &data, accessKey)...)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *managedObjectStorageUserAccessKeyResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data userAccessKeyModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	var serviceUUID, username, accessKeyID string
+	if err := utils.UnmarshalID(data.ID.ValueString(), &serviceUUID, &username, &accessKeyID); err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to unmarshal managed object storage user access key ID",
+			utils.ErrorDiagnosticDetail(err),
+		)
+		return
+	}
+
+	if err := r.client.DeleteManagedObjectStorageUserAccessKey(ctx, &request.DeleteManagedObjectStorageUserAccessKeyRequest{
+		ServiceUUID: serviceUUID,
+		Username:    username,
+		AccessKeyID: accessKeyID,
+	}); err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to delete managed object storage user access key",
+			utils.ErrorDiagnosticDetail(err),
+		)
+		return
+	}
+}
+
+func (r *managedObjectStorageUserAccessKeyResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
 }
