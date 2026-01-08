5eb0647767faed705f6c2f3a2d42b88d82e5ba53
Francisco Serrano <francisco.serrano@upcloud.com>
2026-01-08 17:17:45 +0200

    add plan modifier to policy description

--- internal/service/managedobjectstorage/policy.go
+++ internal/service/managedobjectstorage/policy.go
@@ -15,6 +15,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/boolplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -63,6 +64,9 @@ func (r *managedObjectStoragePolicyResource) Schema(_ context.Context, _ resourc
 			"attachment_count": schema.Int64Attribute{
 				Description: "Attachment count.",
 				Computed:    true,
+				PlanModifiers: []planmodifier.Int64{
+					int64planmodifier.UseStateForUnknown(),
+				},
 			},
 			"created_at": schema.StringAttribute{
 				Description: "Creation time.",
@@ -79,7 +83,7 @@ func (r *managedObjectStoragePolicyResource) Schema(_ context.Context, _ resourc
 				Description: "Description of the policy. This property is immutable after creation.",
 				Optional:    true,
 				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.UseStateForUnknown(),
+					UseStateAfterCreate(),
 				},
 			},
 			"document": schema.StringAttribute{
