aaaf3df40eca2230afa6e637857528dc7b657412
Ville Välimäki <110451292+villevsv-upcloud@users.noreply.github.com>
2026-09-23 07:34:35 +0300

    chore: update note

--- examples/resources/upcloud_firewall_rules/resource.tf
+++ examples/resources/upcloud_firewall_rules/resource.tf
@@ -1,6 +1,6 @@
 # The following example defines a server and then links the server to a single firewall rule. 
 # The list of firewall rules applied to the server can be expanded by providing additional server_firewall_rules blocks.
-# Note: upcloud_firewall_rules configures legacy public firewall rules directly on the server.
+# Note: upcloud_firewall_rules configures public firewall rules on the server.
 
 resource "upcloud_server" "example" {
   firewall = true
